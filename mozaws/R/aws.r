4000756d194bba3903e02693d36a81be08b3c7e4
Saptarshi Guha <saptarshi.guha@gmail.com>
2015-06-02 06:17:54 -0700

    remove old man files

--- mozaws/R/aws.r
+++ mozaws/R/aws.r
@@ -1,3 +1,18 @@
+##' Takes seconds and converts to human readable format
+##' @param secs is the number of seconds
+##' @return a string
+##' @export
+secondsToString <- function(secs,rnd=2){
+  Round <- function(a,b){
+    format(round(a,b),nsmall=2)
+  }
+  if(secs<60) sprintf("%s seconds",secs)
+  else if(secs<60*60) sprintf("%s minutes",Round(secs/60,rnd))
+  else if(secs< 86400) sprintf("%s hours", Round(secs/(60*60),rnd))
+  else if(secs< (86400*30)) sprintf("%s days",Round(secs/(86400),rnd))
+  else if(secs< (86400*365)) sprintf("%s months",Round(secs/(86400*30),rnd))
+  else  sprintf("%s years",Round(secs/(86400*365),rnd))
+}
         
 #' Initialize the AWS System
 #' @param ec2key this is your EC2 key that you created in the EC2 AWS Console
@@ -18,7 +33,8 @@ aws.init <- function(ec2key=NULL,localpubkey=NULL,optsmore=NULL){
     opts <- aws.options()
     if(is.null(ec2key)){
         aaws <- if(is.null(optsmore$aws)) "aws" else optsmore$aws
-        opts$ec2key <- system(sprintf("%s ec2 describe-key-pairs --output text --query 'KeyPairs[0].KeyName'",aaws),intern=TRUE)
+        opts$ec2key <- system(sprintf("%s ec2 describe-key-pairs --output text --query 'KeyPairs[0].KeyName'",aaws)
+                             ,intern=TRUE)
         if(opts$ec2key =="None") stop("Could not find a key for the region")
         message(sprintf("Using first key found: %s",opts$ec2key))
     }
@@ -49,6 +65,7 @@ presult <- function(s){
 #' @param emrfs turns on emrfs and consistency
 #' @param customscript override options
 #' @param wait TRUE or FALSE for waiting. If FALSE, the function returns immediately or waits
+#' @param spark if TRUE will start a spark interactive cluster. If a spark cluster is used, you cannot write interactive hadoop jobs.
 #' @details The arguments \code{hadoopops, timeout, customscript} can
 #' also be set in options. If \code{wait} is FALSE, the function will
 #' return immediately and can be monitored using
@@ -68,7 +85,7 @@ presult <- function(s){
 #' }
 #' @export
 aws.clus.create <- function(name=NULL, workers=NULL,master=NULL,hadoopops=NULL,timeout=NULL,verbose=FALSE,emrfs=FALSE
-                           ,customscript=NULL,wait=FALSE){
+                           ,customscript=NULL,wait=FALSE,spark=FALSE){
     awsOpts <- aws.options()
     checkIfStarted()
     getWT <- function(s,k){
@@ -89,13 +106,17 @@ aws.clus.create <- function(name=NULL, workers=NULL,master=NULL,hadoopops=NULL,t
     timeout <- if(is.null(timeout)) timeout else awsOpts$timeout
     if(emrfs) emrfs="--emrfs Consistent=true" else emrfs=""
     if(!is.na(customscript)){
-        customscript <- sprintf("Type=CUSTOM_JAR,Name=CustomJAR,ActionOnFailure=CONTINUE,Jar=s3://elasticmapreduce/libs/script-runner/script-runner.jar,Args=['s3://{{s3buk}}/run.user.script.sh','%s']", customscript)
+        customscript <- sprintf("Type=CUSTOM_JAR,Name=CustomJAR,ActionOnFailure=CONTINUE,Jar=s3://elasticmapreduce/libs/script-runner/script-runner.jar,Args=['s3://{{s3buk}}/run.user.script.sh','%s']"
+                              , customscript)
     }else customscript=""
+    sparkb=if(spark) {
+        "Path='s3://support.elasticmapreduce/spark/install-spark','Install Spark','-v,1.2.1.a' Path='s3://telemetry-spark-emr/telemetry.sh','SetupSpark'"
+    } else ""
     args <- list(awscli = awsOpts$awscli, amiversion=awsOpts$amiversion,loguri=awsOpts$loguri
-                ,name=name, ec2key=awsOpts$ec2key,mastertype=master[[2]], numworkers=workers[[1]]
+                ,name=name, ec2key=awsOpts$ec2key,mastertype=master[[2]], numworkers=workers[[1]],spark=sparkb
                 ,workertype=workers[[2]],hadoopargs=hadoopargs, uusser=isn(awsOpts$user,isn(Sys.getenv("USERNAME"),"MysteriousI"))
                 ,timeout=awsOpts$timeout, pubkey=awsOpts$localpubkey,emrfs=emrfs,customscript=customscript,s3buk=awsOpts$s3bucket)
-    template = "{{awscli}} emr create-cluster {{emrfs}} --tags user='{{uusser}}' crtr=rmozaws-1 --visible-to-all-users  --ami-version '{{amiversion}}' --log-uri '{{loguri}}'  --name '{{name}}' --enable-debugging --ec2-attributes KeyName='{{ec2key}}' --instance-groups InstanceGroupType=MASTER,InstanceCount=1,InstanceType={{mastertype}}  InstanceGroupType=CORE,InstanceCount={{numworkers}},InstanceType={{workertype}}  --bootstrap-actions Path='s3://elasticmapreduce/bootstrap-actions/configure-hadoop',Args=[{{hadoopargs}}] Path='s3n://{{s3buk}}/kickstartrhipe.sh',Args=['--public-key,{{pubkey}}','--timeout,{{timeout}}'] --steps Type=CUSTOM_JAR,Name=CustomJAR,ActionOnFailure=CONTINUE,Jar=s3://elasticmapreduce/libs/script-runner/script-runner.jar,Args=['s3://{{s3buk}}/final.step.sh'] {{customscript}}"
+    template = "{{awscli}} emr create-cluster {{emrfs}} --tags user='{{uusser}}' crtr=rmozaws-1 --visible-to-all-users  --ami-version '{{amiversion}}' --log-uri '{{loguri}}'  --name '{{name}}' --enable-debugging --ec2-attributes KeyName='{{ec2key}}' --instance-groups InstanceGroupType=MASTER,InstanceCount=1,InstanceType={{mastertype}}  InstanceGroupType=CORE,InstanceCount={{numworkers}},InstanceType={{workertype}}  --bootstrap-actions Path='s3://elasticmapreduce/bootstrap-actions/configure-hadoop',Args=[{{hadoopargs}}] Path='s3n://{{s3buk}}/kickstartrhipe.sh',Args=['--public-key,{{pubkey}}','--timeout,{{timeout}}'] {{spark}} --steps Type=CUSTOM_JAR,Name=CustomJAR,ActionOnFailure=CONTINUE,Jar=s3://elasticmapreduce/libs/script-runner/script-runner.jar,Args=['s3://{{s3buk}}/final.step.sh'] {{customscript}}"
     template <- infuse(template, args)
     if(verbose) cat(sprintf("%s\n",template))
     res <- presult(system(template, intern=TRUE))
@@ -209,22 +230,29 @@ print.awsCluster <- function(r){
     }else ""
     awsconsole=sprintf("https://us-west-2.console.aws.amazon.com/elasticmapreduce/home?region=us-west-2#cluster-details:%s",r$Id)
     temp <- infuse("Cluster ID: {{clid}}
-This Information As of: {{dd}} mins ago
-Name: '{{name}}'
-State: {{state}}
-Started At : {{started}}
-Message: {{currently}}
-IP: {{dns}}
-SOCKS: ssh -ND 8157 hadoop@{{dns}} (and use FoxyProxy for Firefox or SwitchySharp for Chrome)
-Rstudio: http://{{dns}}
-Shiny: http://{{dns}}:3838
-JobTrakcer: http://{{dns}}:9026 (needs a socks)
-Master Type: {{master}} (and is running: {{isrunning}})
-Core Nodes: {{nworker}} of  {{ workerstype }}
+This Information ss of: {{dd}} ago
+Name        : '{{name}}'
+State       : {{state}}
+Started At  : {{started}}
+Message     : {{currently}}
+IP          : {{dns}}
+SSH         : ssh hadoop@{{dns}} (assuming you aws.init(localpub=your-pub-key) else ssh -i path-to-aws-pem-file hadoop@{{dns}}
+SOCKS       : ssh -ND 8157 hadoop@{{dns}} (and use FoxyProxy for Firefox or SwitchySharp for Chrome)
+Rstudio     : http://{{dns}}
+Shiny       : http://{{dns}}:3838
+JobTracker  : http://{{dns}}:9026 (needs a socks)
+Spark UI    : http://{{dns}}: ( if spark == TRUE )
+Master Type : {{master}} (and is running: {{isrunning}})
+Core Nodes  : {{nworker}} of  {{ workerstype }}
 {{gtext}}
 
 {{awsconsole}}
-",list(clid=r$Id, dd=as.integer(round(Sys.time() - r$timeupdated,0)),name=name, state=state, started=started, currently=currently, dns=dns, master=master['type'], isrunning=as.logical(master['running']), nworker=workers.core$'running', workerstype=workers.core$type,gtext=gtext,awsconsole=awsconsole))
+"
+,list(clid  =r$Id, dd=secondsToString(as.numeric(Sys.time() - r$timeupdated,"secs")),name=name
+, state     =state, started=started, currently=currently
+, dns       =dns, master=master['type'],isrunning=as.logical(master['running'])
+, nworker   =workers.core$'running', workerstype=workers.core$type,gtext=gtext
+,awsconsole =awsconsole))
     cat(temp)
 }
 
@@ -273,7 +301,8 @@ aws.spot.price <- function(type=as.character(aws.options()$inst.type['worker']),
     awsOpts <- aws.options()
     checkIfStarted()
     since <- strftime(Sys.time()-hrsInPast*3600,"%Y-%m-%dT%H:%M:%S.000Z")
-    temp <- presult(system(infuse("{{awscli}} ec2 describe-spot-price-history --product-description \"Linux/UNIX (Amazon VPC)\" --instance-types {{type}} --start-time {{start}}", awscli=awsOpts$awscli,type=type, start=since),intern=TRUE))
+    temp <- presult(system(infuse("{{awscli}} ec2 describe-spot-price-history --product-description \"Linux/UNIX (Amazon VPC)\" --instance-types {{type}} --start-time {{start}}"
+         , awscli=awsOpts$awscli,type=type, start=since),intern=TRUE))
     f <- rbindlist(lapply(temp$SpotPriceHistory, as.data.table))
     f$Timestamp <- as.POSIXct(f$Timestamp,format="%Y-%m-%dT%H:%M:%S.000Z")
     f$SpotPrice <- as.numeric(f$SpotPrice)
@@ -314,7 +343,8 @@ aws.modify.groups <- function(cl,n,groupid=NULL, type=as.character(aws.options()
         spotq <- sprintf("BidPrice=%s,", p)
         name= if(is.null(name)) sprintf("Spot %s", name) else name
     }
-    temp=infuse("{{awscli}} emr add-instance-groups --cluster-id  {{clid}} --instance-groups InstanceCount={{n}},{{spotq}}InstanceGroupType=task,InstanceType={{mtype}},Name='{{foo}}'", awscli=awsOpts$awscli,clid=cl$Id,n=as.integer(n),spotq=spotq, mtype=as.character(type),foo=name)
+    temp=infuse("{{awscli}} emr add-instance-groups --cluster-id  {{clid}} --instance-groups InstanceCount={{n}},{{spotq}}InstanceGroupType=task,InstanceType={{mtype}},Name='{{foo}}'"
+             , awscli=awsOpts$awscli,clid=cl$Id,n=as.integer(n),spotq=spotq, mtype=as.character(type),foo=name)
     l <- presult(system(temp,intern=TRUE))
     aws.clus.info(cl)
 }
