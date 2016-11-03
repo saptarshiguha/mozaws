a635cc0153e9092b29d322de42e53fd33dca3ab3
Saptarshi Guha <saptarshi.guha@gmail.com>
2016-11-03 12:26:29 -0700

    rhipe install fixed

---
+++ bootscriptsAndR/rhipe.r
@@ -0,0 +1,20 @@
+library(Rhipe)
+rhinit()
+RDIST <- "R" 
+m <- rhoptions()$mropts
+m$mapred.reduce.tasks = 50
+m$R_ENABLE_JIT        = 2
+m$R_HOME              = sprintf("%s/R",RDIST)
+m$R_HOME_DIR          = sprintf("./%s/R",RDIST)
+m$R_SHARE_DIR         = sprintf("./%s/R/share",RDIST)
+m$R_INCLUDE_DIR       = sprintf("./%s/R/include",RDIST)
+m$R_DOC_DIR           = sprintf("./%s/R/doc",RDIST)
+m$PATH                = sprintf("./%s/R/bin:./%s/:$PATH",RDIST,RDIST)
+m$LD_LIBRARY_PATH     = sprintf("./%s/:./%s/R/lib:/usr/lib64",RDIST,RDIST)
+rhoptions(HADOOP.TMP.FOLDER = sprintf("/tmp/"))
+rhoptions(runner            = sprintf("./%s/RhipeMapReduce --silent --vanilla",RDIST),
+          zips              = c(sprintf("/%s.tar.gz",RDIST)),
+          HADOOP.TMP.FOLDER = sprintf("/tmp/"),
+          mropts            = m,
+          job.status.overprint =TRUE,
+          write.job.info    =TRUE)
--- bootscriptsAndR/setupRhipe.sh
+++ bootscriptsAndR/setupRhipe.sh
@@ -15,29 +15,7 @@ echo "export HADOOP_CONF_DIR=/etc/hadoop/conf" >> /home/hadoop/.bash_profile
 aws s3 cp s3://mozilla-metrics/user/sguha/tmp/R.tar.gz /tmp/
 hadoop dfs -put  /tmp/R.tar.gz /
 
-cat << EOF > /home/hadoop/rhipe.r
-library(Rhipe)
-rhinit()
-RDIST <- "R" 
-m <- rhoptions()$mropts
-m$mapred.reduce.tasks = 50
-m$R_ENABLE_JIT        = 2
-m$R_HOME              = sprintf("%s/R",RDIST)
-m$R_HOME_DIR          = sprintf("./%s/R",RDIST)
-m$R_SHARE_DIR         = sprintf("./%s/R/share",RDIST)
-m$R_INCLUDE_DIR       = sprintf("./%s/R/include",RDIST)
-m$R_DOC_DIR           = sprintf("./%s/R/doc",RDIST)
-m$PATH                = sprintf("./%s/R/bin:./%s/:$PATH",RDIST,RDIST)
-m$LD_LIBRARY_PATH     = sprintf("./%s/:./%s/R/lib:/usr/lib64",RDIST,RDIST)
-rhoptions(HADOOP.TMP.FOLDER = sprintf("/tmp/"))
-rhoptions(runner            = sprintf("./%s/RhipeMapReduce --silent --vanilla",RDIST),
-          zips              = c(sprintf("/%s.tar.gz",RDIST)),
-          HADOOP.TMP.FOLDER = sprintf("/tmp/"),
-          mropts            = m,
-          job.status.overprint =TRUE,
-          write.job.info    =TRUE)
-EOF
-
+wget 
 echo "echo \"Remember to call source('~/rhipe.r') if you want RHIPE\"" >> ~/.bash_profile
 
 ## Download the R packages and copy them into the folder
