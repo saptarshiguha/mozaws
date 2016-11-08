6fa68b2042e725e4d3350f2e7ed992954dffe5e1
Saptarshi Guha <saptarshi.guha@gmail.com>
2016-11-08 07:33:17 -0800

    added file monitoring

--- bootscriptsAndR/monitor.sh
+++ bootscriptsAndR/monitor.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+
 inotifywait -mr  --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' -e close_write ~/public_html/tmp/ | while read date time dir file; do
-    rsync -zvrae 'ssh -p 9999' ~/public_html/tmp/ localhost:/Users/sguha/Sites/tmp/
+    rsync -zvrae 'ssh -p 9999' ~/r localhost:/Users/sguha/Sites/tmp/
 done
 
 
