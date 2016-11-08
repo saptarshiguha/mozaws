cc3f49eb80f66eb8ea0d8bbd177216ea26674757
Saptarshi Guha <saptarshi.guha@gmail.com>
2016-11-08 08:00:58 -0800

    new sync

--- bootscriptsAndR/monitor.sh
+++ bootscriptsAndR/monitor.sh
@@ -2,7 +2,7 @@
 
 
 inotifywait -mr  --timefmt '%d/%m/%y %H:%M' --format '%T %w %f' -e close_write ~/r | while read date time dir file; do
-    rsync -zvrae 'ssh -p 9999' ~/r localhost:/Users/sguha/Sites/tmp/
+    rsync -zvrae 'ssh -p 9999' ~/r sguha@localhost:/Users/sguha/Sites/tmp/
 done
 
 
