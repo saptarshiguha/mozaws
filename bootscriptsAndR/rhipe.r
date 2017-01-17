c8e37027afc733c117436a73e741f52711fd6568
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2017-01-17 13:02:09 -0800

    Update rhipe.r

--- bootscriptsAndR/rhipe.r
+++ bootscriptsAndR/rhipe.r
@@ -46,5 +46,32 @@ rsp <- function(o,cnames=NULL,r=NA){
     if(!is.null(cnames))  setnames(x,cnames)                                           
 }
     
-        
+print.sparky <- function(l){
+    x <- l[[1]]
+    pN <- function(s) prettyNum(s,big.mark=",",scientific=FALSE,preserve.width="none")
+    inpRec <- pN(x$counters$`Map-Reduce Framework`['Map input records',][[1]])
+    oupRec <- pN(x$counters$`Map-Reduce Framework`['Reduce output records',][[1]])
+    oupsize <- pN(x$counters$`File Output Format Counters `['Bytes Written',][[1]])
+    ifo <- paste(l[[2]]$lines$rhipe_input_folder,collapse=":")
+    ifo <-sprintf( "%s ...",substr(ifo,1, min(100,nchar(ifo))))
+    lfo <- l[[2]]$lines$rhipe_output_folder
+    cat(paste(strwrap(sprintf("Job read from %s, wrote to %s, input records where %s and wrote %s records occupying %s bytes.
+Use $join() to join an asynchronous job, $output to get the output location, and $collect() to get results\n\n",
+            ifo, lfo, inpRec, oupRec, oupsize),width=120),collapse="\n"))
+}
+
+rh <- (function(input,setup){
+    cs = function(...){
+        l <- list(...)
+        if(is.null(l$input)) l$input <- input
+        if(is.null(l$setup)) l$setup <- setup
+        if(!is.null(l$reduce)) l$reduce <- CS
+        l$read=FALSE
+        r <- do.call(rhwatch,l)
+        class(r) <- c(class(r),"sparky")
+        list(result=r, join=function() rhjoin(r),output=r[[2]]$lines$rhipe_output_folder, collect=function(s) rhread(r[[2]]$lines$rhipe_output_folder))
+    }
+})(I,E)
+  
+                                              
 setwd("~/r")
