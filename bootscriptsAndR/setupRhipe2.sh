a50053ba45dca0b69f12b1eb9d6b344466ac670c
Saptarshi Guha <saptarshi.guha+github@gmail.com>
2017-04-06 14:15:10 -0700

    Update setupRhipe2.sh

--- bootscriptsAndR/setupRhipe2.sh
+++ bootscriptsAndR/setupRhipe2.sh
@@ -5,9 +5,7 @@
 ## The lines here did not install stuff into the home folder 
 ## and therefore need to be installed again.
 
-wget http://cbs.centos.org/kojifiles/packages/protobuf/2.5.0/10.el7.centos/x86_64/protobuf-2.5.0-10.el7.centos.x86_64.rpm
-wget http://cbs.centos.org/kojifiles/packages/protobuf/2.5.0/10.el7.centos/x86_64/protobuf-devel-2.5.0-10.el7.centos.x86_64.rpm
-wget http://cbs.centos.org/kojifiles/packages/protobuf/2.5.0/10.el7.centos/x86_64/protobuf-compiler-2.5.0-10.el7.centos.x86_64.rpm
+cd ~/
 sudo yum -y install protobuf-2.5.0-10.el7.centos.x86_64.rpm protobuf-compiler-2.5.0-10.el7.centos.x86_64.rpm protobuf-devel-2.5.0-10.el7.centos.x86_64.rpm
 
 
